1a5142e479b43a1ff92af871747d9046ae3dfa8b
Bairui Su <subairui@icloud.com>
2025-08-23 08:07:39 -0400

    Support clear (#16)
    * Support clear
    
    * Fix clear

---
+++ examples/clear.js
@@ -0,0 +1,24 @@
+const code = `{
+  let count = 10;
+  
+  const timer = setInterval(() => { 
+    clear();
+    if (count-- <= 0) {
+      clearInterval(timer);
+      setTimeout(() => {
+        clear();
+      }, 1000);
+    } else {
+      doc(count);
+    }
+  }, 1000);
+
+  doc(count);
+
+  invalidation.then(() => clearInterval(timer));
+}`;
+
+export const clear = {
+  code,
+  name: "Clear",
+};
