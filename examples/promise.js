acffca92277dc7b37433742e072e6e8f0130e0a0
Bairui Su <subairui@icloud.com>
2025-08-23 00:13:31 -0400

    Show Pending… (#15)

--- examples/promise.js
+++ examples/promise.js
@@ -1,6 +1,12 @@
 const code = `const a = doc(new Promise(resolve => setTimeout(() => resolve(1), 2000)));
 
-const b = doc(a + 1);`;
+const b = doc(a + 1);
+
+{
+  doc(1);
+  doc(a + 2);
+  doc(b + 3);
+}`;
 
 export const promise = {
   name: "Promise",
