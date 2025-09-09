3258c05633b5a42f0af6660a3fc7a48b998594d2
pearmini <subairui@icloud.com>
2025-09-09 16:28:45 -0400

    Update Mandelbrot set examples

--- app/examples/mandelbrot-set.recho.js
+++ app/examples/mandelbrot-set.recho.js
@@ -7,9 +7,29 @@
  * @thumbnail_start 23
  */
 
+/**
+ * ============================================================================
+ * =                            Mandelbrot Set                                =
+ * ============================================================================
+ *
+ * I'm always fascinated by fractals. It's truly amazing that fractals allows
+ * us generate complex and beautiful patterns from simple rules or code.
+ *
+ * The Mandelbrot set[1] is one of the most famous fractals. So I want to use
+ * Recho to draw a text-based Mandelbrot set to demonstrate we can create
+ * something appealing with just few lines of code in Recho.
+ *
+ * I'm not going to explain the details of what the Mandelbrot set is or how to
+ * implement it here. The whole idea is that we apply certain math formulas to
+ * each point of a plane iteratively until the point satisfies certain
+ * conditions. Then we can color the point based on the number of iterations.
+ * We're using characters here of course!
+ */
+
 const cols = 80;
 const rows = 30;
 const maxIter = 80;
+const colors = ["·", "*", "o", "O", "@"];
 
 //➜
 //➜
@@ -53,7 +73,8 @@ const maxIter = 80;
         if (a * a + b * b > 4) break;
         i++;
       }
-      output += i === maxIter ? "0" : " ";
+      const index = ~~((i / maxIter) * (colors.length - 1));
+      output += colors[index];
     }
     output += y === rows - 1 ? "" : "\n";
   }
@@ -63,3 +84,25 @@ const maxIter = 80;
 function map(x, d0, d1, r0, r1) {
   return r0 + ((r1 - r0) * (x - d0)) / (d1 - d0);
 }
+
+/**
+ * Again, you don't need to completely understand the code above for now. If
+ * you find textual outputs can be interesting and creative by this example,
+ * that's the point!
+ *
+ * If you're really curious about Mandelbrot set, here are some examples that
+ * I made with Charming.js[2] you may find interesting:
+ *
+ * - Multibrot Set: https://observablehq.com/d/fc2cfd9ae9e7524c
+ * - Multibrot Set Table: https://observablehq.com/d/3028c0d5655345e3
+ * - Multibrot Set Transition: https://observablehq.com/d/c040d3db33c0033e
+ * - Zoomable Mandelbrot Set (Canvas): https://observablehq.com/d/2e5bdd2365236c2d
+ * - Zoomable Mandelbrot Set (WebGL): https://observablehq.com/d/cfe263c1213334e3
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *                              References
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * [1] https://en.wikipedia.org/wiki/Mandelbrot_set
+ * [2] https://charmingjs.org/
+ */
