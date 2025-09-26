74c29241a41a77bf1952f185ea7c4284b7d962c9
pearmini <subairui@icloud.com>
2025-09-26 19:24:52 -0400

    Responsivee docs page

--- app/Sidebar.js
+++ app/Sidebar.js
@@ -3,11 +3,18 @@ import Link from "next/link";
 import {usePathname} from "next/navigation";
 import {cn} from "./cn.js";
 
-export function Sidebar({docs}) {
+export function Sidebar({docs, onLinkClick}) {
   const pathname = usePathname();
   const isActive = (slug) => pathname.startsWith(`/docs/${slug}`);
+  
+  const handleLinkClick = () => {
+    if (onLinkClick) {
+      onLinkClick();
+    }
+  };
+
   return (
-    <ul className={cn("h-full", "overflow-auto px-4 border-r border-gray-200 pt-2")} style={{width: "320px"}}>
+    <ul className={cn("h-full", "overflow-auto px-4 w-full")}>
       {docs
         .sort((a, b) => a.order - b.order)
         .map((doc) => (
@@ -15,6 +22,7 @@ export function Sidebar({docs}) {
             href={`/docs/${doc.slug}`}
             key={doc.title}
             className={cn("block px-3 py-2 rounded-md hover:bg-gray-100", isActive(doc.slug) && "bg-gray-100")}
+            onClick={handleLinkClick}
           >
             <li>{doc.title}</li>
           </Link>
