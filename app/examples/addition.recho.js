fdcee9d981d4bf57e15152028bdabb97292153a3
pearmini <subairui@icloud.com>
2025-09-26 08:22:21 -0400

    Add example addition

---
+++ app/examples/addition.recho.js
@@ -0,0 +1,27 @@
+/**
+ * @title Addition
+ * @author Bairui Su
+ * @created 2025-09-26
+ * @pull_request 123
+ * @github pearmini
+ * @thumbnail_start 17
+ */
+
+/**
+ * ============================================================================
+ * =                                Addition                                  =
+ * ============================================================================
+ *
+ * This example shows Recho's core feature: inline echoing. You can `echo` a 
+ * value directly at the point of definition or anywhere in your code.
+ */
+
+const a = 1;
+
+//➜ 2
+const b = echo(a + 1);
+
+const c = a + b;
+
+//➜ 3
+echo(c);
