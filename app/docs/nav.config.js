5ce6bda53c9d92a97d36f50e57263b1b4b674761
pearmini <subairui@icloud.com>
2025-11-09 08:20:53 -0500

    Update state order

--- app/docs/nav.config.js
+++ app/docs/nav.config.js
@@ -61,11 +61,11 @@ export const docsNavConfig = [
       },
       {
         type: "page",
-        slug: "api-inspect",
+        slug: "api-state",
       },
       {
         type: "page",
-        slug: "api-state",
+        slug: "api-inspect",
       },
       {
         type: "page",
