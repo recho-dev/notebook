e1461c0131ae0e67299e0c21033689fa676ea03f
Luyu Cheng <2239547+chengluyu@users.noreply.github.com>
2025-12-15 04:14:20 +0800

    Extract output related logic

--- runtime/index.js
+++ runtime/index.js
@@ -1,28 +1,19 @@
 import {transpileJavaScript} from "@observablehq/notebook-kit";
 import {Runtime} from "@observablehq/runtime";
 import {parse} from "acorn";
-import {group, groups, max} from "d3-array";
+import {group} from "d3-array";
 import {dispatch as d3Dispatch} from "d3-dispatch";
 import * as stdlib from "./stdlib/index.js";
 import {Inspector} from "./stdlib/inspect.js";
-import {OUTPUT_MARK, ERROR_MARK} from "./constant.js";
 import {BlockMetadata} from "../editor/blocks/BlockMetadata.ts";
 import {blockMetadataEffect} from "../editor/blockMetadata.ts";
 import {IntervalTree} from "../lib/IntervalTree.ts";
 import {transpileRechoJavaScript} from "./transpile.js";
-import {table, getBorderCharacters} from "table";
 import {ButtonRegistry, makeButton} from "./controls/button.js";
-
-const OUTPUT_PREFIX = `//${OUTPUT_MARK}`;
-
-const ERROR_PREFIX = `//${ERROR_MARK}`;
+import {addPrefix, makeOutput, OUTPUT_PREFIX, ERROR_PREFIX} from "./output.js";
 
 function uid() {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-}
-
-function isError(value) {
-  return value instanceof Error;
 }
 
 function safeEval(code, inputs, __setEcho__) {
@@ -50,55 +41,6 @@ function debounce(fn, delay = 0) {
     clearTimeout(timeout);
     timeout = setTimeout(() => fn(...args), delay);
   };
-}
-
-function padStringWidth(string) {
-  const lines = string.split("\n");
-  const maxLength = max(lines, (line) => line.length);
-  return lines.map((line) => line.padEnd(maxLength)).join("\n");
-}
-
-function padStringHeight(string, height) {
-  const lines = string.split("\n");
-  const diff = height - lines.length;
-  return lines.join("\n") + "\n".repeat(diff);
-}
-
-function merge(...strings) {
-  const maxHeight = max(strings, (string) => string.split("\n").length);
-  const aligned = strings
-    .map((string) => padStringHeight(string, maxHeight))
-    .map((string) => padStringWidth(string))
-    .map((string) => string.split("\n"));
-  let output = "";
-  for (let i = 0; i < maxHeight; i++) {
-    for (let j = 0; j < aligned.length; j++) {
-      const line = aligned[j][i];
-      output += line;
-      output += j < aligned.length - 1 ? " " : "";
-    }
-    output += i < maxHeight - 1 ? "\n" : "";
-  }
-  return output;
-}
-
-function addPrefix(string, prefix) {
-  const lines = string.split("\n");
-  return lines.map((line) => `${prefix} ${line}`).join("\n");
-}
-
-function withTable(groups) {
-  return groups.length > 1 || groups[0][0] !== undefined;
-}
-
-function columns(data, options) {
-  const values = data[0].slice(1);
-  let output = "";
-  for (let i = 0; i < values.length; i++) {
-    output += values[i];
-    output += i < values.length - 1 ? "\n" : "";
-  }
-  return output;
 }
 
 export function createRuntime(initialCode) {
@@ -155,52 +97,7 @@ export function createRuntime(initialCode) {
         continue;
       }
 
-      // Group values by key. Each group is a row if using table, otherwise a column.
-      const groupValues = groups(values, (v) => v.options?.key);
-
-      // We need to remove the trailing newline for table.
-      const format = withTable(groupValues) ? (...V) => table(...V).trimEnd() : columns;
-
-      // If any value is an error, set the error flag.
-      let error = false;
-
-      // Create a table to store the formatted values.
-      const data = [];
-
-      for (const [key, V] of groupValues) {
-        const values = V.map((v) => v.values);
-
-        // Format the key as a header for table.
-        const row = ["{" + key + "}"];
-
-        for (let i = 0; i < values.length; i++) {
-          const line = values[i];
-          const n = line.length;
-
-          // Each cell can have multiple values. Format each value as a string.
-          const items = line.map((v) => {
-            if (isError(v)) error = true;
-
-            // Disable string quoting for multi-value outputs to improve readability.
-            // Example: echo("a =", 1) produces "a = 1" instead of "a = "1""
-            const options = n === 1 ? {} : {quote: false};
-            const inspector = v instanceof Inspector ? v : new Inspector(v, options);
-            return inspector.format();
-          });
-
-          // Merge all formatted values into a single cell.
-          row.push(merge(...items));
-        }
-
-        data.push(row);
-      }
-
-      // Format the table into a single string and add prefix.
-      const formatted = format(data, {
-        border: getBorderCharacters("ramac"),
-        columnDefault: {alignment: "right"},
-      });
-      const prefixed = addPrefix(formatted, error ? ERROR_PREFIX : OUTPUT_PREFIX);
+      const {output, error} = makeOutput(values);
 
       // The range of line numbers of output lines.
       let outputRange = null;
@@ -210,21 +107,21 @@ export function createRuntime(initialCode) {
 
       // Entry not found. This is a new output.
       if (entry === null) {
-        changes.push({from: start, insert: prefixed + "\n"});
+        changes.push({from: start, insert: output + "\n"});
         outputRange = {from: start, to: start};
       } else {
         const change = changes[entry.data];
-        change.insert = prefixed + "\n";
+        change.insert = output + "\n";
         outputRange = {from: change.from, to: change.to};
       }
 
       // Add this block to the block metadata array.
-      const block = new BlockMetadata(node.type, outputRange, {from: node.start, to: node.end}, node.state.attributes);
+      const block = new BlockMetadata(node.type, outputRange, sourceRange, node.state.attributes);
       block.error = error;
       blocks.push(block);
-
-      blocks.sort((a, b) => a.from - b.from);
     }
+
+    blocks.sort((a, b) => a.from - b.from);
 
     // Attach block positions and attributes as effects to the transaction.
     const effects = [blockMetadataEffect.of(blocks)];
